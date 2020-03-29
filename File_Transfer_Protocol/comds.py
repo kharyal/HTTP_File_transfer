0cd7c0666765d2385cdc89bc1bd2670619249ad6
kharyal <chaitanyajee@gmail.com>
2020-03-30 02:25:50 +0530

    minor enhancements

--- File_Transfer_Protocol/comds.py
+++ File_Transfer_Protocol/comds.py
@@ -1,25 +1,26 @@
 import os
 import time
 
-def send_files_shortlist(arg, file_list, s, depth):
+def send_files_shortlist(arg, file_list, s, pwd):
     l = len(arg)
     if l == 1:
         for i in range(len(file_list)):
-            for d in range(depth):
-                print("   ", end ="")
-                s.send("   ".encode())
-            print("|--", end ="")
-            s.send("|--".encode())
+            print(pwd+str(file_list[i]))
+            s.send((pwd+file_list[i]+"\n").encode())
+            
+            if os.path.isdir(pwd+str(file_list[i])):
+                send_files_shortlist(arg, os.listdir(pwd+str(file_list[i])), s, pwd+str(file_list[i])+'/')
+
+    elif l == 3:
+        for i in range(len(file_list)):
             print(file_list[i])
             s.send((file_list[i]+"\n").encode())
-            
-            if os.path.isdir(file_list[i]):
-                send_files_shortlist(arg, os.listdir("./"+str(file_list[i])), s, depth+1)
-
+            if os.path.isdir(pwd+str(file_list[i])):
+                send_files_shortlist(arg, os.listdir(pwd+str(file_list[i])), s, pwd+str(file_list[i])+'/')
         
 
 def  ind_get(arg, s):
     if arg[0] == 'shortlist':
         file_list = os.listdir()
-        send_files_shortlist(arg,file_list,s,0)
+        send_files_shortlist(arg,file_list,s, "./")
                 
